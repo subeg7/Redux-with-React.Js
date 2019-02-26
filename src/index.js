69b5d4620ae823b075ae68561d032a0e6abf181e
Subeg7 <subeg7@gmail.com>
2019-02-26 13:26:33 +0545

    redux init

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app.jsx';
+import {Provider} from 'react-redux';
 
 ReactDOM.render(
-    <App/>,document.getElementById('root')
+
+    <Provider>
+      <App/>
+    </Provider>
+    ,document.getElementById('root')
 )
